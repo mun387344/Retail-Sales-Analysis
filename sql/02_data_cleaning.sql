a2d1918a8228ab4dafd0b9e8a0aa0d5fb9a04592
Muna Mohammed <muna.muhammed391@gmail.com>
2026-09-22 22:17:26 +0800

    Investigate missing values and product descriptions

--- sql/02_data_cleaning.sql
+++ sql/02_data_cleaning.sql
@@ -3,7 +3,36 @@ FROM online_retail
 WHERE Quantity < 0
 AND InvoiceNo LIKE 'C%';
 
-SELECT *S
+SELECT *
 FROM online_retail
 WHERE Quantity < 0
 AND InvoiceNo NOT LIKE 'C%';
+
+SELECT 
+COUNT(*) FILTER (WHERE InvoiceNo IS NULL) AS missing_invoice,
+COUNT(*) FILTER (WHERE StockCode IS NULL) AS missing_stockcode,
+COUNT (*)FILTER (WHERE description IS NULL) AS missing_description,
+COUNT(*) FILTER (WHERE quantity IS NULL) AS missing_quantity,
+COUNT(*) FILTER (WHERE invoicedate IS NULL) AS missing_invoicedate,
+COUNT(*) FILTER (WHERE unitprice IS NULL) AS missing_unitprice,
+COUNT(*) FILTER (WHERE customerid IS NULL) As missing_customerid,
+COUNT(*) FILTER (WHERE Country IS NULL) AS missing_country
+FROM online_retail;
+
+SELECT *
+FROM online_retail
+WHERE description IS NULL
+LIMIT 20;
+
+SELECT COUNT(*)
+FROM online_retail
+WHERE description IS NULL
+AND UnitPrice = 0;
+
+SELECT COUNT(*)
+FROM online_retail
+WHERE description IS NULL
+AND customerid IS NULL;
+
+SELECT MIN(Quantity),MAX(Quantity)
+FROM online_retail;
